e3a4acfd3f4b1a2d79c7c44ccc9ce43c62d13c65
Rafael Guterres Jeffman <rafasgj@gmail.com>
2016-09-20 19:32:10 -0300

    Modified sizes to cope with all sizes required for icons.

--- create_ios_icons.sh
+++ create_ios_icons.sh
@@ -6,12 +6,11 @@ shift
 ext=`echo ${filename} | awk -F. '{ print $NF }' `
 file=`basename ${filename} .${ext}`
 
-sizes=(512 180 152 120 120 87 80 76 58 40 29)
-names=("iTunesArtwork" "icon-60-3x" "icon-76-2x" "icon-60-2x" "icon-small-40-3x" "icon-small-3x" "icon-small-40-2x" "icon-76" "icon-small-2x" "icon-small-40" "icon-small")
+sizes=(512 180 167 152 120 120 87 80 76 58 40 29 20)
 
 for i in `seq 0 $[${#sizes[*]} - 1]`
 do
     sz=${sizes[i]}
-    tgt="${file}-${names[i]}-${sz}x${sz}.${ext}"
+    tgt="${file}-${sz}.${ext}"
     convert "${filename}" -monitor -resize "${sz}x${sz}" "${tgt}"
 done
