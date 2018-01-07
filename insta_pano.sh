ffecbb318ecaba5ab806191a0f0531193561d675
Rafael Guterres Jeffman <rafasgj@gmail.com>
2018-01-06 23:19:03 -0200

    Modified script to generate 1:1 slices, as is currently required by Instagram form multiple image posts.

--- insta_pano.sh
+++ insta_pano.sh
@@ -11,6 +11,15 @@ sz=`identify ${file} | cut -d" " -f3`
 w=`echo ${sz} | cut -dx -f1`
 h=`echo ${sz} | cut -dx -f2`
 
-slice=`echo "print (100.0/($w/1080))" | python`
+slices=`echo "print $w/1080" | python`
 
-convert $file -crop ${slice}%x100% +repage $output.jpg
+max_w=$[$slices * 1080]
+
+if [ $max_w -ne $w ]
+then
+    tmp=/tmp/`basename ${file}`
+    convert ${file} -resize ${max_w}x${h} -background white -gravity center -extent ${max_w}x${h} -format jpg -quality 100 $tmp
+    file=$tmp
+fi
+
+convert ${file} -crop `echo "print(100.0/$slices)" | python`%x100% +repage $output.jpg
