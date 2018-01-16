41c5276d3ea8a185f465f812bb0363cd57b6f292
Rafael Guterres Jeffman <rafasgj@gmail.com>
2018-01-15 22:46:01 -0200

    Updated script to rescale too big images (vertical size greater than 1080 pixels).

--- insta_pano.sh
+++ insta_pano.sh
@@ -6,20 +6,31 @@ shift
 output=$1
 shift
 
-sz=`identify ${file} | cut -d" " -f3`
+tmp=/tmp/`basename ${file}`
 
+sz=`identify ${file} | cut -d" " -f3`
 w=`echo ${sz} | cut -dx -f1`
 h=`echo ${sz} | cut -dx -f2`
 
-slices=`echo "print $w/1080" | python`
+if [ $h -gt 1080 ]
+then
+    convert ${file} -resize x1080 -gravity center -format jpg $tmp
+    file=$tmp
+    sz=`identify ${file} | cut -d" " -f3`
+    w=`echo ${sz} | cut -dx -f1`
+    h=`echo ${sz} | cut -dx -f2`
+fi 
+
+slices=`echo "print $w/$h" | python`
 
 max_w=$[$slices * 1080]
 
-if [ $max_w -ne $w ]
+if [ $max_w -lt $w ]
 then
-    tmp=/tmp/`basename ${file}`
+    tmp=/tmp/tmp-`basename ${file}`
     convert ${file} -resize ${max_w}x${h} -background white -gravity center -extent ${max_w}x${h} -format jpg -quality 100 $tmp
     file=$tmp
 fi
 
 convert ${file} -crop `echo "print(100.0/$slices)" | python`%x100% +repage $output.jpg
+
